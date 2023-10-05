7ce59d1878f51ec29b3595dcb1c8d48fb2072914
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2023-10-05 10:21:35 +0530

    Add decimals to TOKEN_INFO, stronger return types on ix funcs, successful tests

--- ts/src/constants/tokenConstants.ts
+++ ts/src/constants/tokenConstants.ts
@@ -69,26 +69,31 @@ export const TOKEN_INFO: TokenInfo[] = [
   {
     mint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
     name: "MSOL",
-    oracle: "E4v1BBgoso9s64TQvmyownAVJbhbEPGyzA3qn4n46qj9"
+    oracle: "E4v1BBgoso9s64TQvmyownAVJbhbEPGyzA3qn4n46qj9",
+    decimals: 9
   },
   {
     mint: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
     name: "BONK",
-    oracle: "8ihFLu5FimgTQ1Unh4dVyEHUGodJ5gJQCrQf4KUVB9bN"
+    oracle: "8ihFLu5FimgTQ1Unh4dVyEHUGodJ5gJQCrQf4KUVB9bN",
+    decimals: 5
   },
   {
     mint: "So11111111111111111111111111111111111111112",
     name: "WSOL",
-    oracle: "H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG"
+    oracle: "H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG",
+    decimals: 9
   },
   {
     mint: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
     name: "USDT",
-    oracle: "3vxLXJqLqF3JG5TCbYycbKWRBbCJQLxQmBGCkyqEEefL"
+    oracle: "3vxLXJqLqF3JG5TCbYycbKWRBbCJQLxQmBGCkyqEEefL",
+    decimals: 6
   },
   {
     mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
     name: "USDC",
-    oracle: "Gnt27xtC473ZT2Mw5u8wZ68Z3gULkSTb5DuxJy7eJotD"
+    oracle: "Gnt27xtC473ZT2Mw5u8wZ68Z3gULkSTb5DuxJy7eJotD",
+    decimals: 6
   }
 ]
---
+++ ts/test/v2.ts
@@ -0,0 +1,137 @@
+import * as anchor from "@project-serum/anchor";
+import { SSL } from "../src";
+import { describe, it } from "node:test";
+import { ComputeBudgetProgram, sendAndConfirmTransaction } from "@solana/web3.js";
+import { delay, getDecimalsFromMint } from "../src/public/utils";
+import { assert } from "node:console";
+import { TOKEN_INFO } from "../src/constants";
+
+const fs = require('fs');
+require('dotenv').config();
+
+describe("Main-net tests on SSL-v2 SDK", () => {
+
+    const connection = new anchor.web3.Connection("https://api.mainnet-beta.solana.com");
+
+    const privateKey = fs.readFileSync("/Users/dhrumil/.config/solana/goosefx.json", "utf-8");
+    const userKeypair = anchor.web3.Keypair.fromSecretKey(new Uint8Array(JSON.parse(privateKey)));
+    const wallet =  new anchor.Wallet(userKeypair);
+    console.log("User public key: ", wallet.publicKey);
+
+    const WRAPPED_SOL = new anchor.web3.PublicKey('So11111111111111111111111111111111111111112');
+    const USDC_MAINNET = new anchor.web3.PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+
+    const sslClient = new SSL(connection, wallet.publicKey);
+
+    it("Create a LiquidityAccount for a user", async() => {
+        try {
+            const txPacket = await sslClient.createLiquidityAccountIx({
+                tokenMint: USDC_MAINNET
+            });
+
+            const tx = new anchor.web3.Transaction();
+            
+            for(let ix of txPacket.transactionInfos.ixs) {
+                tx.add(ix);
+            }
+
+            const signature = await sendAndConfirmTransaction(connection, tx,[userKeypair, ...txPacket.transactionInfos.signers], {skipPreflight: true});
+            console.log('Create LiquidityAccount signature: ', signature);
+
+            await delay(5_000);
+
+            const liquidityAccount = await sslClient.program.account.liquidityAccount.fetch(txPacket.liquidityAccountAddr);
+
+            //@ts-ignore
+            assert(liquidityAccount.owner.toString() === userKeypair.publicKey.toString(), "Owner of the account does not match");
+        }
+        catch(err) {
+            console.log("Error; ", err);
+        }
+    });
+
+    it("Deposit some tokens to the vault", async() => {
+        try {
+
+            let usdcDecimals = getDecimalsFromMint(USDC_MAINNET.toString());
+            let amountIn = new anchor.BN(1).mul(new anchor.BN(10 ** usdcDecimals));
+
+            const txPacket = await sslClient.depositIx({
+                tokenMint: USDC_MAINNET,
+                amountIn,
+            })
+
+            const tx = new anchor.web3.Transaction();
+            
+            for(let ix of txPacket.transactionInfos.ixs) {
+                tx.add(ix);
+            }
+
+            const signature = await sendAndConfirmTransaction(connection, tx,[userKeypair, ...txPacket.transactionInfos.signers], {skipPreflight: true});
+            console.log(`Deposit tokens signature: https://solscan.io/tx/${signature}`);
+        }
+        catch(err) {
+            console.log("Error; ", err);
+        }
+    });
+
+    it("Withdraw some tokens to the vault", async() => {
+        try {
+
+            let usdcDecimals = getDecimalsFromMint(USDC_MAINNET.toString());
+            let amountIn = new anchor.BN(1).mul(new anchor.BN(10 ** usdcDecimals));
+
+            const txPacket = await sslClient.withdrawIx({
+                tokenMint: USDC_MAINNET,
+                amountIn,
+            })
+
+            const tx = new anchor.web3.Transaction();
+            
+            for(let ix of txPacket.transactionInfos.ixs) {
+                tx.add(ix);
+            }
+
+            const signature = await sendAndConfirmTransaction(connection, tx,[userKeypair, ...txPacket.transactionInfos.signers], {skipPreflight: true});
+            console.log(`Withdraw tokens signature: https://solscan.io/tx/${signature}`);
+        }
+        catch(err) {
+            console.log("Error; ", err);
+        }
+    });
+
+    it('Swap some tokens', async() => {
+        try {
+            let usdcDecimals = getDecimalsFromMint(USDC_MAINNET.toString());
+            let amountIn = new anchor.BN(1).mul(new anchor.BN(10 ** usdcDecimals));
+
+            let tokenMintIn = USDC_MAINNET;
+            let tokenMintOut = WRAPPED_SOL;
+
+            const txPacket = await sslClient.swapIx({
+                tokenMintIn,
+                tokenMintOut,
+                amountIn
+            });
+
+            const tx = new anchor.web3.Transaction();
+
+            const extraUnitsIx = ComputeBudgetProgram.setComputeUnitLimit({units: 1_000_000});
+            tx.add(extraUnitsIx);
+
+            for(let ix of txPacket.transactionInfos.preIxs) {
+                tx.add(ix);
+            }
+
+            for(let ix of txPacket.transactionInfos.ixs) {
+                tx.add(ix);
+            }
+
+            const signature = await sendAndConfirmTransaction(connection, tx, [userKeypair, ...txPacket.transactionInfos.signers], {skipPreflight: true});
+            console.log(`Swap USDC to WSOL signature: https://solscan.io/tx/${signature}`);
+        }
+        catch(err) {
+            console.log("Error; ", err);
+        }
+    });
+});
